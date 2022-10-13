4a68dffe9d25c2398afecf502f550e254052654b
Jivn Shet <107192090+jivnshet@users.noreply.github.com>
2022-10-13 18:16:50 +0530

    Add files via upload

---
+++ Fullstack_blockchain_course_JS_Assignment_09.js
@@ -0,0 +1,32 @@
+// JavaScript Assignment 9 PDF
+
+// Question 1.
+
+// a) Is the InnerFunction() a closure?
+//--> As the InnerFunction is wrapped inside OuterFunction, InnerFunction is a closure
+
+
+// b) What is output of this program
+//--> 100
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 2.
+
+//--> Closure referes to the child function wrapped inside parent function, the child function has access to parent function's scope.
+//--> Scope refers to the context area, in which the variables have access. The access can be local which restricts the area to access the variable, other access which is global allows variable to access the overall area for the context.
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 3.
+
+// When a parent function wraps a child function inside which is also called closure, the child function has access to parent function variables/ constants and functions and and this access of inner/ child function is called lexical scope
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 4.
+
+Output:
+3
+3
+3
