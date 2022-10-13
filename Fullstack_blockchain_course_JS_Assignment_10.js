45511ceb8d4e342d716ec1b3bb87c4b4d882de3f
Jivn Shet <107192090+jivnshet@users.noreply.github.com>
2022-10-13 20:53:56 +0530

    Add files via upload

---
+++ Fullstack_blockchain_course_JS_Assignment_10.js
@@ -0,0 +1,46 @@
+// JavaScript Assignment 9 PDF
+
+// Question 1.
+
+//--> Both aren't the same.
+// Higher order functions can take another function as an arument or can return a function.
+//--> A callback is a function which is passed as an argument to another function.
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 2.
+
+// .filter() takes in a function as a parameter to check a condition, so as it is taking another function as a parameter .filter() is is a Higher order function.
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 3.
+
+//--> So lets say we have a calculator program:
+
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+  myCallback(sum);
+}
+
+myCalculator(5, 5, myDisplayer);
+
+//--> So myDisplayer is a callback function and myCalculator is a higher order function
+
+// -----------*-----------*-----------*-----------*-----------*-----------
+
+// Question 4.
+
+// a.
+// Output:
+// Hello John
+// Hello Tina
+// Hello Kale
+// Hello Max
+
+// b.
+//--> useFunction is a higher order function
